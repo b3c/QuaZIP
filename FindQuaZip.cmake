b599a3db61866507f7424d0e7b9d1d0de24f7e18
mgiacomoni <m.giacomoni@scsitaly.com>
2012-07-04 13:09:10 +0200

    chnaged path for include and lib directories

--- FindQuaZip.cmake
+++ FindQuaZip.cmake
@@ -1,7 +1,12 @@
 find_package(Qt4)
-find_path(QuaZip_INCLUDE_DIR quazip.h ${CMAKE_INSTALL_PREFIX}/include/quazip ${CMAKE_INSTALL_PREFIX}/include /usr/include/quazip /usr/local/include/quazip ${QT_INCLUDE_DIR}/quazip ${QT_INCLUDE_DIR} ${QUAZIP_DIR}/include/quazip ${QUAZIP_DIR}/quazip ${QUAZIP_DIR}/include)
-find_library(QuaZip_LIBRARY NAMES quazip PATHS ${CMAKE_INSTALL_PREFIX}/lib64 ${CMAKE_INSTALL_PREFIX}/lib ${CMAKE_INSTALL_PREFIX}/Library/Frameworks ${QUAZIP_DIR}/lib64 ${QUAZIP_DIR}/lib ${QUAZIP_DIR}/quazip ${QUAZIP_DIR})
+find_path(QuaZip_INCLUDE_DIR quazip.h ${CMAKE_INSTALL_PREFIX}/include/quazip ${CMAKE_INSTALL_PREFIX}/include /usr/include/quazip /usr/local/include/quazip ${QT_INCLUDE_DIR}/quazip ${QT_INCLUDE_DIR} ${QuaZIP_SOURCE_DIR}/include/quazip ${QuaZIP_SOURCE_DIR}/quazip ${QuaZIP_SOURCE_DIR}/include)
+find_library(QuaZip_LIBRARY NAMES quazip PATHS ${CMAKE_INSTALL_PREFIX}/lib64 ${CMAKE_INSTALL_PREFIX}/lib ${CMAKE_INSTALL_PREFIX}/Library/Frameworks ${QuaZIP_DIR}/lib64 ${QuaZIP_DIR}/lib ${QuaZIP_DIR}/quazip ${QuaZIP_DIR})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(quazip DEFAULT_MSG QuaZip_LIBRARY QuaZip_INCLUDE_DIR)
 set(QuaZip_LIBRARIES ${QuaZip_LIBRARY})
+
+include_directories("${QT_PLUGINS_DIR}/../src/3rdparty/zlib/")
+# Add include directories needed to use QtSOAP.
+INCLUDE_DIRECTORIES(${QuaZip_INCLUDE_DIR})
+
 mark_as_advanced(QuaZip_LIBRARY QuaZip_INCLUDE_DIR)
