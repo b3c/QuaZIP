2e9beb6d1f7b5d47aa95cd12e25845541404039d
mgiacomoni <m.giacomoni@scsitaly.com>
2012-10-23 14:02:04 +0200

    changed path for library

--- FindQuaZip.cmake
+++ FindQuaZip.cmake
@@ -1,6 +1,6 @@
 find_package(Qt4)
 find_path(QuaZip_INCLUDE_DIR quazip.h ${CMAKE_INSTALL_PREFIX}/include/quazip ${CMAKE_INSTALL_PREFIX}/include /usr/include/quazip /usr/local/include/quazip ${QT_INCLUDE_DIR}/quazip ${QT_INCLUDE_DIR} ${QuaZIP_SOURCE_DIR}/include/quazip ${QuaZIP_SOURCE_DIR}/quazip ${QuaZIP_SOURCE_DIR}/include)
-find_library(QuaZip_LIBRARY NAMES quazip PATHS ${CMAKE_INSTALL_PREFIX}/lib64 ${CMAKE_INSTALL_PREFIX}/lib ${CMAKE_INSTALL_PREFIX}/Library/Frameworks ${QuaZIP_DIR}/lib64 ${QuaZIP_DIR}/lib ${QuaZIP_DIR}/quazip ${QuaZIP_DIR})
+find_library(QuaZip_LIBRARY NAMES quazip PATHS ${CMAKE_INSTALL_PREFIX}/lib64 ${CMAKE_INSTALL_PREFIX}/lib ${CMAKE_INSTALL_PREFIX}/Library/Frameworks ${QuaZIP_DIR}/lib64 ${QuaZIP_DIR}/lib ${QuaZIP_DIR}/quazip/Debug ${QuaZIP_DIR}/quazip/Relase ${QuaZIP_DIR})
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(quazip DEFAULT_MSG QuaZip_LIBRARY QuaZip_INCLUDE_DIR)
 set(QuaZip_LIBRARIES ${QuaZip_LIBRARY})
